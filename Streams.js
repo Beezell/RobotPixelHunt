e58356031400ea8ba43bfb2c6c200ea0c7c2e724
Beezell <abigael.castillo@live.fr>
2023-06-30 11:41:19 +0200

    first commit

---
+++ Streams.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Streams = void 0;
+//Objet Streams pour le Game
+class Streams {
+    constructor(streamer, title) {
+        this.streamer = streamer;
+        this.title = title;
+    }
+}
+exports.Streams = Streams;
